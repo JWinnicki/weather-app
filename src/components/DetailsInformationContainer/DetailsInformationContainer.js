47b029bf391570cbd9f286425883334621e39bd1
unknown <jwinnicki2@gmail.com>
2020-10-12 17:29:09 +0200

    card visual update

--- src/components/DetailsInformationContainer/DetailsInformationContainer.js
+++ src/components/DetailsInformationContainer/DetailsInformationContainer.js
@@ -5,26 +5,24 @@ import windIcon from '../../assets/windArrow.svg';
 import SVGBackground from '../SVGBackground/SVGBackground';
 
 
-const DetailsInformationContainer = ({pressure, humidity, windSpeed, windDirection}) => {
-    return (
-        <ul className={styles.DetailsContainerList}>
-            <li className={styles.DetailsContainerListItem}>
-                <p className={styles.DetailsContainerListItemText}>Pressure: {pressure} hPa</p>
-            </li>
-            <li className={styles.DetailsContainerListItem}>
-                <p className={styles.DetailsContainerListItemText}>Humidity: {humidity} %</p>
-            </li>
-            <li className={styles.DetailsContainerListItem}>
-                <p className={styles.DetailsContainerListItemText}>Wind Speed: {windSpeed} m/s</p>
-            </li>
-            <li className={styles.DetailsContainerListItem}>
-                <p className={styles.DetailsContainerListItemText}>Wind Direction:</p>
-                <div className={styles.DetailsContainerListItemIcon}>
-                    <SVGBackground icon={windIcon} rotate={windDirection}/>
-                </div>
-            </li>
-        </ul>
-    );
-}
+const DetailsInformationContainer = ({pressure, humidity, windSpeed, windDirection}) => (
+    <ul className={styles.DetailsContainerList}>
+        <li className={styles.DetailsContainerListItem}>
+            <p className={styles.DetailsContainerListItemText}>Pressure: {pressure} hPa</p>
+        </li>
+        <li className={styles.DetailsContainerListItem}>
+            <p className={styles.DetailsContainerListItemText}>Humidity: {humidity} %</p>
+        </li>
+        <li className={styles.DetailsContainerListItem}>
+            <p className={styles.DetailsContainerListItemText}>Wind Speed: {windSpeed} m/s</p>
+        </li>
+        <li className={styles.DetailsContainerListItem}>
+            <p className={styles.DetailsContainerListItemText}>Wind Direction:</p>
+            <div className={styles.DetailsContainerListItemIcon}>
+                <SVGBackground icon={windIcon} rotate={windDirection}/>
+            </div>
+        </li>
+    </ul>
+);
 
 export default DetailsInformationContainer;
